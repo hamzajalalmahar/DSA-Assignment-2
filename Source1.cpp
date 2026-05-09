08ad0b26dad06c048b2fbe3166373a2d0d4e73b7
Minhal Ahmad <minhal8803@gmail.com>
2026-05-09 21:52:55 +0500

    adding the main function

--- Source1.cpp
+++ Source1.cpp
@@ -131,3 +131,59 @@ void Queue::totalPages()
     cout << "\nTotal Pending Pages: " << total << "\n";
 }
 
+int main()
+{
+    Queue q;
+    int choice, jobId, pages;
+
+    do
+    {
+        cout << "\n========== Printer Queue System ==========\n";
+        cout << "1. Add Print Job\n";
+        cout << "2. Print Job\n";
+        cout << "3. Show Next Job\n";
+        cout << "4. Display All Jobs\n";
+        cout << "5. Show Total Pages\n";
+        cout << "6. Exit\n";
+
+        cout << "\nEnter Choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "\nEnter Job ID: ";
+            cin >> jobId;
+            cout << "Enter Pages: ";
+            cin >> pages;
+            q.enqueue(jobId, pages);
+            break;
+
+        case 2:
+            q.dequeue();
+            break;
+
+        case 3:
+            q.peek();
+            break;
+
+        case 4:
+            q.display();
+            break;
+
+        case 5:
+            q.totalPages();
+            break;
+
+        case 6:
+            cout << "\nExiting Program...\n";
+            break;
+
+        default:
+            cout << "\nInvalid Choice!\n";
+        }
+
+    } while (choice != 6);
+
+    return 0;
+}
